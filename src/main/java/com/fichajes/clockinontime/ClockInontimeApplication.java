726a6d58703e6d02196b502ee0844f95dd412e22
BELÉN PULIDO RIVERO <belen.pulido@ikeasi.com>
2021-06-15 10:58:43 +0100

    base project

---
+++ src/main/java/com/fichajes/clockinontime/ClockInontimeApplication.java
@@ -0,0 +1,13 @@
+package com.fichajes.clockinontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockInontimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockInontimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fichajes/clockinontime/ClockInontimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fichajes.clockinontime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockInontimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
